b8953c357c1aff3079228d95d055fcef265bd567
Deny Herianto <deny.hrnt@gmail.com>
2025-08-19 17:18:21 +0700

    chore: add remaining og:property

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,6 +5,13 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta property="og:image" content="{{ asset('images/logo_logo.png') }}" />
+    <meta property="og:url" content="https://www.90sautoworks.com/" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="90's Autoworks" />
+    <meta property="og:description" content="90's Autoworks adalah industri layanan perawatan otomotif serba ada" />
+    <meta property="og:logo" content="{{ asset('images/logo_logo.png') }}" />
+    <meta property="title" content="90's Autoworks" />
+    <meta property="description" content="90's Autoworks adalah industri layanan perawatan otomotif serba ada" />
     <link rel="preconnect" href="https://fonts.gstatic.com" />
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&amp;display=swap" rel="stylesheet" />
     <link href="https://api.fontshare.com/v2/css?f[]=clash-grotesk@400,300,500&amp;display=swap" rel="stylesheet" />
@@ -13,10 +20,10 @@
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/logo.png') }}" />
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.3/dist/cdn.min.js" defer="defer"></script>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
-    
+
     <!-- AOS CSS -->
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    
+
     @stack('css')
     @stack('head')
 </head>
@@ -25,7 +32,7 @@
     <div style="font-family: Montserrat;">
         @yield('content')
     </div>
-    
+
     <!-- AOS JavaScript -->
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script>
@@ -36,7 +43,7 @@
             mirror: false
         });
     </script>
-    
+
     @stack('script')
 </body>
 
